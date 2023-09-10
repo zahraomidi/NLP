e86ef37cf1667abc573105eacd8fa9e4ab1b0d9b
Zahra Omidi <zahra.omidi216@gmail.com>
2023-09-10 14:28:04 -0500

    the skeleton

---
+++ LM/hw1.py
@@ -0,0 +1,99 @@
+import argparse
+import math
+import random
+from nltk.tokenize import sent_tokenize, word_tokenize
+from typing import List
+from typing import Tuple
+from typing import Generator
+
+
+# Generator for all n-grams in text
+# n is a (non-negative) int
+# text is a list of strings
+# Yields n-gram tuples of the form (string, context), where context is a tuple of strings
+def get_ngrams(n: int, text: List[str]) -> Generator[Tuple[str, Tuple[str, ...]], None, None]:
+    pass
+
+
+# Loads and tokenizes a corpus
+# corpus_path is a string
+# Returns a list of sentences, where each sentence is a list of strings
+def load_corpus(corpus_path: str) -> List[List[str]]:
+    pass
+
+
+# Builds an n-gram model from a corpus
+# n is a (non-negative) int
+# corpus_path is a string
+# Returns an NGramLM
+def create_ngram_lm(n: int, corpus_path: str) -> 'NGramLM':
+    pass
+
+
+# An n-gram language model
+class NGramLM:
+    def __init__(self, n: int):
+        self.n = n
+        self.ngram_counts = {}
+        self.context_counts = {}
+        self.vocabulary = set()
+
+    # Updates internal counts based on the n-grams in text
+    # text is a list of strings
+    # No return value
+    def update(self, text: List[str]) -> None:
+        pass
+
+    # Calculates the MLE probability of an n-gram
+    # word is a string
+    # context is a tuple of strings
+    # delta is an float
+    # Returns a float
+    def get_ngram_prob(self, word: str, context: Tuple[str, ...], delta= .0) -> float:
+        pass
+
+    # Calculates the log probability of a sentence
+    # sent is a list of strings
+    # delta is a float
+    # Returns a float
+    def get_sent_log_prob(self, sent: List[str], delta=.0) -> float:
+        pass
+
+    # Calculates the perplexity of a language model on a test corpus
+    # corpus is a list of lists of strings
+    # delta is a float
+    # Returns a float
+    def get_perplexity(self, corpus: List[List[str]], delta=0.) -> float:
+        pass
+
+    # Samples a word from the probability distribution for a given context
+    # context is a tuple of strings
+    # delta is an float
+    # Returns a string
+    def generate_random_word(self, context: Tuple[str, ...], delta=.0) -> str:
+        pass
+
+    # Generates a random sentence
+    # max_length is an int
+    # delta is a float
+    # Returns a string
+    def generate_random_text(self, max_length: int, delta=.0) -> str:
+        pass
+
+
+def main(corpus_path: str, delta: float, seed: int):
+    trigram_lm = create_ngram_lm(3, corpus_path)
+    s1 = 'God has given it to me, let him who touches it beware!'
+    s2 = 'Where is the prince, my Dauphin?'
+
+    print(trigram_lm.get_sent_log_prob(word_tokenize(s1)))
+    print(trigram_lm.get_sent_log_prob(word_tokenize(s2)))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="CS6320 HW1")
+    parser.add_argument('corpus_path', nargs="?", type=str, default='warpeace.txt', help='Path to corpus file')
+    parser.add_argument('delta', nargs="?", type=float, default=.0, help='Delta value used for smoothing')
+    parser.add_argument('seed', nargs="?", type=int, default=82761904, help='Random seed used for text generation')
+    args = parser.parse_args()
+    random.seed(args.seed)
+    main(args.corpus_path, args.delta, args.seed)
